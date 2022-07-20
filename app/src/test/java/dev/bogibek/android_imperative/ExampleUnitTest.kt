fa0e6609288d2a08b256de3c731be73cde7299a4
BogibekDev <bogibekdev@gmail.com>
2022-07-20 17:00:41 +0000

    unit tests added for ApiTVShowDetail

--- app/src/test/java/dev/bogibek/android_imperative/ExampleUnitTest.kt
+++ app/src/test/java/dev/bogibek/android_imperative/ExampleUnitTest.kt
@@ -1,10 +1,7 @@
 package dev.bogibek.android_imperative
 
 import dev.bogibek.android_imperative.di.AppModule
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Test
@@ -43,14 +40,35 @@ class ExampleUnitTest {
     @Test
     fun checkTVShowListSize() = runTest {
         val response = AppModule().tvShowService().apiTVShowPopular(1)
-        val tvShowPopular=response.body()
-        assertEquals(20,tvShowPopular!!.tv_shows.size)
+        val tvShowPopular = response.body()
+        assertEquals(20, tvShowPopular!!.tv_shows.size)
     }
+
     @Test
     fun checkFirstTVShowStatus() = runTest {
         val response = AppModule().tvShowService().apiTVShowPopular(1)
-        val tvShows=response.body()!!.tv_shows
-        val tvShow=tvShows[0]
-        assertEquals("Running",tvShow.status)
+        val tvShows = response.body()!!.tv_shows
+        val tvShow = tvShows[0]
+        assertEquals("Running", tvShow.status)
+    }
+
+
+    @Test
+    fun `checking Arrow episode Count`() = runTest {
+        val response = AppModule().tvShowService().apiTVShowDetails(29560)
+        val tvShowDetail = response.body()
+        assertEquals(170, tvShowDetail!!.tvShow.episodes.size)
+    }
+
+    @Test
+    fun `Check country`() = runTest {
+        val response = AppModule().tvShowService().apiTVShowDetails(29560)
+        assertEquals("US", response.body()?.tvShow?.country)
+    }
+
+    @Test
+    fun `is there sci-fi in genres`() = runTest {
+        val response = AppModule().tvShowService().apiTVShowDetails(29560)
+        assertEquals("Action",response.body()?.tvShow?.genres!![1])
     }
 }
